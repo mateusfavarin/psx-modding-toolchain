c6a2834159446f21803c4a071ad91b785d6275b3
dhern023 <dhern023@ucr.edu>
2023-07-08 05:36:03 -0400

    Catch trimbin suffix bug an finish updating to use pathlib

--- tools/trimbin/trimbin.py
+++ tools/trimbin/trimbin.py
@@ -1,12 +1,25 @@
-import sys
+"""
+Called by nugget/common.mk
+TODO: Move to the mod-bulider
+"""
+import logging
 import os
+import pathlib
+import sys
+
+# import _files
+
+logger = logging.getLogger(__name__)
 
 def trimbin(filename: str, target_path: str, offset_file: str) -> None:
+    logger.debug("filename", filename)
+    logger.debug("target_path", target_path)
+    logger.debug("offset_file", offset_file)
     arr = bytearray()
     with open(filename, "rb") as file:
         arr = file.read()
 
-    extension = filename.split(".")[1]
+    extension = pathlib.Path(filename).suffix.replace(".", "")
     with open(offset_file, "r") as file:
         for line in file:
             line = line.split()
@@ -18,12 +31,13 @@ def trimbin(filename: str, target_path: str, offset_file: str) -> None:
         file.write(arr)
 
     new_name = extension + ".bin"
-    new_filename = target_path+new_name
-    if os.path.isfile(new_filename):
-        os.remove(new_filename)
+    new_filename = pathlib.Path(target_path) / new_name
+    if new_filename.exists():
+        new_filename.unlink()
     os.rename(filename, new_filename)
 
 def main() -> None:
     trimbin(sys.argv[1], sys.argv[2], sys.argv[3])
 
-main()
+if __name__ == "__main__":
+    main()
