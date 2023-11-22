17df53faef78c3d9a70b49567684e1c2b53ef5d8
Superstarxalien <ernesto_vallenilla@hotmail.com>
2023-11-22 10:52:16 -0400

    fix plugin.py in example folders

--- games/Example_CrashTeamRacing/plugins/plugin.py
+++ games/Example_CrashTeamRacing/plugins/plugin.py
@@ -5,14 +5,14 @@ def warning() -> None:
     print("[Plugin] WARNING: ctr-tools not found. Please download the tools from https://github.com/CTR-tools/CTR-tools/releases")
     print("rename the folder to ctr-tools and put it in Example_CrashTeamRacing/plugins/")
 
-def extract(plugin_path: str, game_path: str) -> None:
-    if os.path.isdir(plugin_path + "ctr-tools"):
+def extract(plugin_path: str, game_path: str, version: None) -> None:
+    if os.path.join(plugin_path, "ctr-tools"):
         os.system(plugin_path + "ctr-tools/bigtool.exe " + game_path + "BIGFILE.BIG")
     else:
         warning()
 
-def build(plugin_path: str, game_path: str) -> None:
-    if os.path.isdir(plugin_path + "ctr-tools"):
+def build(plugin_path: str, game_path: str, version: None) -> None:
+    if os.path.join(plugin_path, "ctr-tools"):
         bigpath = os.getcwd() + "/bigfile"
         shutil.copytree(game_path + "bigfile", bigpath)
         os.system(plugin_path + "ctr-tools/bigtool.exe " + game_path + "bigfile.txt")
