f3d50ba4ab6f807bbcf9a3144e60308d9dd39a25
dhern023 <dhern023@ucr.edu>
2024-02-19 14:20:22 -0500

    Add test for spyro c-struct before refactor

--- tools/mod-builder/tests/test_image.py
+++ tools/mod-builder/tests/test_image.py
@@ -3,6 +3,12 @@ import pytest
 
 import image
 
+@pytest.fixture(scope="module")
+def image_directory():
+    """ hard-coded directory """
+    dirname = pathlib.Path(__file__).resolve().parents[3] / "games" / "Example_CrashTeamRacing" / "mods" / "TextureReplacement"
+    yield dirname
+
 cases_fnames = ( # fname stems
     "SPYRO_368_216_1_251_44_26_4",
     "ZERO_1_2_3_4_5_6_7",
@@ -11,28 +17,24 @@ cases_fnames = ( # fname stems
 def test_check_naming_convention(string):
     assert image.Image.check_naming_convention(string)
 
-def test_create_images():
-    dirname = pathlib.Path(__file__).resolve().parents[3] / "games" / "Example_CrashTeamRacing" / "mods" / "TextureReplacement"
-    print(dirname)
-    # pytest.set_trace()
-    assert image.create_images(dirname) == 0
-    # count_images = 0
-    # for root, _, files in os.walk(directory):
-    #     for filename in files:
-    #         if filename[-4:].lower() == ".png":
-    #             count_images += 1
-    #             path = root + filename
-    #             img = Image(path)
-    #             if img.is_valid():
-    #                 imgs.append(img)
-    #                 img.img2psx()
-    #                 if img.pil_img.mode == "PA":
-    #                     img.clut.add_indexed_colors(img.pil_img)
-    # return count_images
+def test_create_images(image_directory):
+    """
+    TODO: Replace hard-coded directory
+    """
+    print(image_directory)
+    assert image.create_images(image_directory) == 1
 
-def test_as_c_struct():
+def test_as_c_struct(image_directory):
     """
     TODO: create a fake image file
     """
-    fname_image_texture = "SPYRO_368_216_1_251_44_26_4"
-    instance = image.Image(fname_image_texture)
+    fname_image_texture = "SPYRO_368_216_1_251_44_26_4.png"
+    instance = image.Image(image_directory / "newtex" / fname_image_texture)
+    string_test = instance.as_c_struct()
+    string_actual = ""
+    with open(pathlib.Path.cwd() / "tests" / "spyro_c_struct.c", "r") as f:
+        string_actual = f.read()
+    print(string_actual)
+    print(string_test)
+    assert instance.is_valid()
+    assert string_actual == string_test
