68efdc29d8ff4fe868e7ecfb222252a778b66a76
jjamfed <justan333@gmail.com>
2024-06-03 17:29:06 -0700

    updated to accept game version param

--- tools/startup/plugin.py
+++ tools/startup/plugin.py
@@ -1,5 +1,5 @@
-def extract(plugin_path: str, game_path: str) -> None:
+def extract(plugin_path: str, game_path: str, game_version: str) -> None:
     pass
 
-def build(plugin_path: str, game_path: str) -> None:
+def build(plugin_path: str, game_path: str, game_version: str) -> None:
     pass
